89608f5b08e86f965ef749fadbb9d97858401633
Wanding Zhou <zhouwanding@gmail.com>
2023-04-26 12:05:09 -0400

    minor, message omitted

--- sesame/v1.18/build.sh
+++ sesame/v1.18/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 for f in sesame QC nonhuman modeling inferences KYCG; do
-  wzbuild1_vignette.R ../../../sesame/vignettes/$f.Rmd
-  cp ../../../sesame/vignettes/$f.html .
+  wzbuild1_vignette.R ../../../sesame_1.18/vignettes/$f.Rmd
+  cp ../../../sesame_1.18/vignettes/$f.html .
 done
 
 wzbuild1_vignette.R supplemental.Rmd
